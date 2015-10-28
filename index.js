9224b44052fdf8e87c5f5c4067b6f65ec460ed12
Arjen Smit <arjen@alnitak.nl>
2015-10-28 22:39:05 +0100

    Added a gitignore and some basic script

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 'use strict';
 
-var Stream = require('stream');
-var Path = require('path');
+// through2 is a thin wrapper around node transform streams
+var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
 
-var gulpCriticalAgainst = function gulpCriticalAgainst (obj) {
+// Consts
+const PLUGIN_NAME = 'gulp-prefixer';
 
-  var stream = new Stream.Transform({objectMode: true});
-
+function prefixStream(prefixText) {
+  var stream = through();
+  stream.write(prefixText);
   return stream;
-};
+}
 
+// Plugin level function(dealing with files)
+function gulpCriticalAgainst(prefixText) {
+
+  if (!prefixText) {
+    throw new PluginError(PLUGIN_NAME, 'Missing prefix text!');
+  }
+  prefixText = new Buffer(prefixText); // allocate ahead of time
+
+  // Creating a stream through which each file will pass
+  return through.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      // return empty file
+      return cb(null, file);
+    }
+    if (file.isBuffer()) {
+      file.contents = Buffer.concat([prefixText, file.contents]);
+    }
+    if (file.isStream()) {
+      file.contents = file.contents.pipe(prefixStream(prefixText));
+    }
+
+    cb(null, file);
+
+  });
+
+}
+
+// Exporting the plugin main function
 module.exports = gulpCriticalAgainst;
